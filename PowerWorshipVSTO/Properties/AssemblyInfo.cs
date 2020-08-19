cd7502e0cad61f4f1a836aebbc3cdd04e66dae13
George Dewar <george@dewar.co.nz>
2020-08-19 20:28:24 +1200

    Set installer version from Assembly Version, ignore Publish version, allow upgrade

--- PowerWorshipVSTO/Properties/AssemblyInfo.cs
+++ PowerWorshipVSTO/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerWorshipVSTO")]
+[assembly: AssemblyTitle("PowerWorship")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PowerWorshipVSTO")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("PowerWorship")]
+[assembly: AssemblyCopyright("Copyright © George Dewar 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
+[assembly: AssemblyVersion("1.1.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
